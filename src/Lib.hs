397de100b98229b0227981dd00f27971b0993de0
DESKTOP-H26ORQ8\JiangHaochen <jianghc724@foxmail.com>
2017-01-10 21:26:26 +0800

    simple and or

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,19 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Lib 
-    ( someFunc
-    ) where
-        
+-- 若在 ghci 中执行，需 :set -XOverloadedStrings
+
+module Lib where
+
 import Control.Applicative
 import Data.Attoparsec.Text
 import Data.Functor
-import Data.Text
-
-
-
-someFunc :: IO()
-someFunc = putStrLn "someFunc"
-
-
 
 data Expr
     = FalseLit
@@ -24,7 +16,7 @@ data Expr
     deriving Show
     
 exprParser :: Parser Expr
-exprParser = falseParser <|> trueParser <|> notParser
+exprParser = falseParser <|> trueParser <|> notParser <|> andParser <|> orParser
 
 falseParser :: Parser Expr
 falseParser = lexeme $ string "False" $> FalseLit
@@ -40,7 +32,70 @@ notParser = do
     lexeme $ char ')'
     return (Not expr)
     
+andParser :: Parser Expr
+andParser = do
+    lexeme $ char '('
+    expr1 <- exprParser
+    lexeme $ string "and"
+    expr2 <- exprParser
+    lexeme $ char ')'
+    return (And expr1 expr2)
+    
+orParser :: Parser Expr
+orParser = do
+    lexeme $ char '('
+    expr1 <- exprParser
+    lexeme $ string "or"
+    expr2 <- exprParser
+    lexeme $ char ')'
+    return (Or expr1 expr2)
+    
 lexeme :: Parser a -> Parser a
 lexeme p = do
     skipSpace
     p
+
+eval :: Expr -> Bool
+eval FalseLit = False
+eval TrueLit = True
+eval (Not p) = not $ eval p
+eval (And p q) = (eval p) && (eval q) 
+eval (Or p q) = (eval p) || (eval q)
+
+-- designed for parseOnly
+--   :: Data.Attoparsec.Text.Parser a
+--      -> Data.Text.Internal.Text -> Either String a
+evalWithErrorThrowing :: Either String Expr -> String
+evalWithErrorThrowing (Left errStr) = "not a valid bool expr: " ++ errStr
+evalWithErrorThrowing (Right expr) = show $ eval expr
+
+defMain :: IO ()
+defMain = do
+    putStrLn $ show $ parseOnly notParser "(not True)"
+    putStrLn $ show $ parse notParser "(not True)"
+    putStrLn "-------"
+    putStrLn $ show $ parseOnly notParser "(nXXX True)"
+    putStrLn $ show $ parse notParser "(nXXX True)"
+    putStrLn "-------"
+    putStrLn $ show $ parseOnly notParser "(not Tr"
+    putStrLn $ show $ parse notParser "(not Tr"
+    putStrLn "-------"
+    putStrLn $ show $ parseOnly notParser "(not True)   MORE"
+    putStrLn $ show $ parse notParser "(not True)   MORE"
+    putStrLn "--------------"
+    putStrLn $ show $ parseOnly exprParser "(not True)"
+    putStrLn $ show $ parse exprParser "(not True)"
+    putStrLn "-------"
+    putStrLn $ show $ parseOnly exprParser "(nXXX True)"
+    putStrLn $ show $ parse exprParser "(nXXX True)"
+    putStrLn "-------"
+    putStrLn $ show $ parseOnly exprParser "(not Tr"
+    putStrLn $ show $ parse exprParser "(not Tr"
+    putStrLn "-------"
+    putStrLn $ show $ parseOnly exprParser "(not True)   MORE"
+    putStrLn $ show $ parse exprParser "(not True)   MORE"
+    putStrLn "--------------"
+    putStrLn $ show $ evalWithErrorThrowing $ parseOnly exprParser "((not True) or False)"
+    putStrLn $ show $ evalWithErrorThrowing $ parseOnly exprParser "(not Tr"
+    putStrLn $ show $ evalWithErrorThrowing $ parseOnly exprParser "(nXXX True)"
+    putStrLn $ show $ evalWithErrorThrowing $ parseOnly exprParser "(not True)   MORE"
